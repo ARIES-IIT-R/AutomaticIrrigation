0ca18f378aeb81a4cf78fa399b729f107140e108
Electronics Section IITR <ecsectioniitr@gmail.com>
2016-07-22 18:04:20 +0530

    v1

---
+++ Arduino Code/arduino_code1.ino
@@ -0,0 +1,130 @@
+
+      int valve_pin2 = 7;
+      int valve_pin3 = 8;
+void setup()
+{
+     Serial.begin(9600);
+     pinMode(valve_pin2, OUTPUT);
+     pinMode(valve_pin3, OUTPUT);
+}
+
+void loop() 
+{
+      int output[6];
+      int h5,h6,h7,h8,h9,h10;
+  //valve2
+for(int i=4;i<8;)
+
+{
+      h5=analogRead(A0);
+      {
+        if(h5>900)
+        {
+          Serial.println("Dry soil");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("Humid soil");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h6=analogRead(A1);
+      {
+        if(h6>900)
+        {
+          Serial.println("Dry soil");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("Humid soil");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h7=analogRead(A2);
+      {
+        if(h7>900)
+        {
+          Serial.println("Dry soil");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("Humid soil");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      
+}
+ if((output[4]&&output[5]) ||  (output[5]&&output[6]) || (output[6]&&output[4]))
+   {
+       digitalWrite(valve_pin2,LOW);
+  }
+
+//valve3
+for(int i=6;i<10;)
+{
+      h8=analogRead(A3);
+      {
+        if(h8>900)
+        {
+          Serial.println("Dry soil");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("Humid soil");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h9=analogRead(A4);
+      {
+        if(h9>900)
+        {
+          Serial.println("Dry soil");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("Humid soil");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h10=analogRead(A5);
+      {
+        if(h10>900)
+        {
+          Serial.println("Dry soil");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("Humid soil");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+}
+if((output[7]&&output[8]) ||  (output[8]&&output[9]) || (output[9]&&output[7]))
+        {
+       digitalWrite(valve_pin3,LOW);
+        }
+
+
+}
---
+++ Arduino Code/arduino_code2.ino
@@ -0,0 +1,83 @@
+void setup() 
+{
+  Serial.begin(9600); 
+}
+void loop() 
+{
+      int output[4];
+      int valve_pin1;
+      int h1,h2,h3,h4;
+//for valve1
+for(int i=0;i<4;)
+{
+      h1=analogRead(A0);
+     
+      {
+        if(h1<300)
+        {
+          Serial.println(h1);
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println(h1);
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h2=analogRead(A1);
+      {
+        if(h2<300)
+        {
+          Serial.println(h2);
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println(h2);
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h3=analogRead(A2);
+      {
+        if(h3>900)
+        {
+          Serial.println(h3);
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println(h3);
+           output[i]=0;
+           delay(1500);
+        }
+      }
+      i++;
+      h4=analogRead(A3);
+      {
+        if(h4>900)
+        {
+          Serial.println("1");
+          output[i]=1;
+          delay(1500);
+        }
+        else
+        {
+           Serial.println("1");
+           output[i]=0;
+           delay(1500);
+        }
+      }
+}
+if((output[0]&&output[1]&&output[2]) ||  (output[1]&&output[2]&&output[3]) || (output[2]&&output[3]&&output[0]) || (output[3]&&output[0]&&output[1]))
+   {
+       digitalWrite(valve_pin1,HIGH);
+   }
+  
+}
